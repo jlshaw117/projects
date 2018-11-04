d4bce1ff98556d4860d7320c47ebdd5ca2fe7827
Jerrik Shaw <jlshaw117@gmail.com>
2018-11-04 15:04:32 -0800

    Add skeleton for Piece class

--- chess/pieces/piece.rb
+++ chess/pieces/piece.rb
@@ -1,12 +1,36 @@
 class Piece
   attr_reader :symbol
 
-  def initialize
-
+  def initialize(color, board, pos)
+    @color = color
+    @board = board
+    @pos = pos
   end
 
   def to_s
     "#{symbol}"
+  end
+
+  def empty?
+
+  end
+
+  def valid_moves
+
+  end
+
+  def pos=(val)
+
+  end
+
+  def symbol
+    raise "Not Implemented"
+  end
+
+  private
+
+  def move_into_check(end_pos)
+
   end
 
 end
